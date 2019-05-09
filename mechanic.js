a8a0dbc89b6f787599bd6a48f37aa39033234806
elboso <31boso@gmail.com>
2019-05-09 17:05:14 +0200

    Add score and victory/defeat message

--- mechanic.js
+++ mechanic.js
@@ -8,19 +8,24 @@ const scissor = 'https://image.flaticon.com/icons/svg/124/124717.svg';
 // content
 const computerImage = document.querySelector('#computerImg');
 const roundResult = document.querySelector('#roundResult');
+const finalWord = document.querySelector('#finalWord');
 
-const list = [rock,paper, scissor];
+let playerScore = 0;  // keep track of playRound wins
+let computerScore = 0;
+
+const list = [rock,paper,scissor];
 let playerSelection = 1;
 
 // check buttons then start a game if clicked
 const boutons = document.querySelectorAll('button');
 
 boutons.forEach(bouton => {
-  bouton.addEventListener('click', ()=>console.log(bouton.name));
   bouton.addEventListener('click',
   ()=>{playerSelection = bouton.name});
   bouton.addEventListener('click', ()=>{roundResult.textContent=(playRound(playerSelection, computerPlay()));
   });
+  bouton.addEventListener('click', ()=>console.log(`player : ${playerScore} I computer ${computerScore}`));
+  bouton.addEventListener('click', final );
 
 });
 
@@ -37,28 +42,51 @@ function playRound (pSelect, cSelect) {
   }
   else if (pSelect==0) {
     if (cSelect==1) {
+      computerScore += 1;
       return 'Paper beats Rock, you lost'
+
     }
     if (cSelect==2) {
+      playerScore += 1;
       return 'Rock beats Scissor, you won !'
     }
   }
 
   else if (pSelect==1) {
     if (cSelect==0) {
+      playerScore += 1;
       return 'Paper beats Rock, you won!'
     }
     if (cSelect==2) {
+      computerScore += 1;
       return 'Scissor beats Paper, you lost'
     }
   }
 
   else if (pSelect==2) {
     if (cSelect==0) {
+      computerScore += 1;
       return 'Rock beats Scissor, you lost'
     }
     if (cSelect==1) {
+      playerScore += 1;
       return 'Scissor beats Paper, you won!'
     }
   }
 };
+
+function final () {
+  if (playerScore>4) {
+    finalWord.textContent = "YOU HAVE WON";
+    finalWord.classList.add('victory');
+    playerScore = 0;
+    computerScore = 0;
+  }
+  else if (computerScore>4) {
+    finalWord.textContent = "YOU HAVE LOST";
+    finalWord.classList.add('defeat');
+    playerScore = 0;
+    computerScore = 0;
+  }
+  else return;
+}
